27b8a8ea80fd66c211becbfc9aeb26a85bed7652
Fittipaldih <fittipaldi.h@gmail.com>
2022-11-15 13:43:31 -0300

    popup tarjeta y direccion en mi cuenta

---
+++ js/micuenta.js
@@ -0,0 +1,22 @@
+
+const ventanaNuevaDireccion = document.querySelector(".ventanaNuevaDireccion")
+
+const abrirDireccion = () => {
+    ventanaNuevaDireccion.showModal();
+}
+
+const cerrarDireccion = () => {
+    ventanaNuevaDireccion.close();
+};
+
+const ventanaNuevaTarjeta = document.querySelector(".ventanaNuevaTarjeta");
+
+const abrirTarjeta = () => {
+    ventanaNuevaTarjeta.showModal();
+}
+
+const cerrarTarjeta = () => {
+    ventanaNuevaTarjeta.close();
+}
+
+
